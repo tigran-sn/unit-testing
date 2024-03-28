43a5a3603762cf5df8c4b7bdc6451eac13fef179
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2024-03-28 23:46:00 +0400

    [Feature] Added unit tests related to appearence and loading status for ButtonComponent s

--- projects/ui-kit/src/lib/components/button/button.component.spec.ts
+++ projects/ui-kit/src/lib/components/button/button.component.spec.ts
@@ -23,4 +23,43 @@ describe('ButtonComponent', () => {
     // Will work on every platform that Angular supports
     expect(el.query(By.css('.button-label'))).toBeDefined();
   });
+
+  describe('ButtonComponent appearance', () => {
+    it('should have solid appearance by default', () => {
+      expect(el.classes['solid-button']).toBe(true);
+      expect(el.nativeElement.classList.contains('solid-button')).toBe(true);
+      expect(el.nativeElement).toContain('solid-button');
+      expect(el.query(By.css('.solid-button'))).toBeDefined();
+    });
+
+    it('should have stroked appearance', () => {
+      fixture.componentInstance.appearance = 'stroked';
+      fixture.detectChanges();
+      expect(el.query(By.css('.stroked-button'))).toBeDefined();
+    });
+
+    it('should have dashed appearance', () => {
+      fixture.componentInstance.appearance = 'dashed';
+      fixture.detectChanges();
+      expect(el.query(By.css('.dashed-button'))).toBeDefined();
+    });
+  });
+
+  describe('ButtonComponent loading state', () => {
+    it('should not have loading state by default', () => {
+      expect(el.query(By.css('.loader'))).toBeNull();
+    });
+
+    it('should have loading state', () => {
+      fixture.componentInstance.loading = true;
+      fixture.detectChanges();
+      expect(el.query(By.css('.loader'))).toBeDefined();
+    });
+
+    it('should have loading state', () => {
+      fixture.componentInstance.loading = false;
+      fixture.detectChanges();
+      expect(el.query(By.css('.loader'))).toBeNull();
+    });
+  });
 });
