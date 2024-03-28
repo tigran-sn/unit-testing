7b95017a60e56031f08d6c5b107c8102f9efb7ba
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2024-03-28 21:52:41 +0400

    [feature] Implemented basic test structure debugElement and nativeElements for ButtonComponent

--- projects/ui-kit/src/lib/components/button/button.component.spec.ts
+++ projects/ui-kit/src/lib/components/button/button.component.spec.ts
@@ -1,18 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ButtonComponent } from './button.component';
 import { ButtonModule } from './button.module';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('ButtonComponent', () => {
   let fixture: ComponentFixture<ButtonComponent>;
+  let el: DebugElement;
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [ButtonModule],
     });
     fixture = TestBed.createComponent(ButtonComponent);
+    el = fixture.debugElement;
     fixture.detectChanges(); // Initial CD
   });
 
   it('should create the component', () => {
-    expect(true).toBeTruthy();
+    // Will work fine only in environments that has either emulated DOM or real DOM like at browser
+    // expect(fixture.nativeElement.querySelector('.button-label')).toBeDefined();
+
+    // Will work on every platform that Angular supports
+    expect(el.query(By.css('.button-label'))).toBeDefined();
   });
 });
